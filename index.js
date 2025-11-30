ad90444b65c8f01b58f63e7530399c5f70a8369e
moe913 <masoodm197@gmail.com>
2025-11-29 23:30:43 -0500

    Fix Mobile Sign Out: Add onclick handler

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('Common Investor v24 Loaded');
+console.log('Common Investor v25 Loaded');
 // ===== Utilities =====
 const $ = (sel) => document.querySelector(sel);
 const $$ = (sel) => Array.from(document.querySelectorAll(sel));
