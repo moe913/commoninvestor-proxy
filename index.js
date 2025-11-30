b508553981c39379ba6c84e253feed382e124c5e
moe913 <masoodm197@gmail.com>
2025-11-29 23:46:51 -0500

    Fix Mobile Hub: Style saved lists and community items

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('Common Investor v29 Loaded');
+console.log('Common Investor v30 Loaded');
 // ===== Utilities =====
 const $ = (sel) => document.querySelector(sel);
 const $$ = (sel) => Array.from(document.querySelectorAll(sel));
