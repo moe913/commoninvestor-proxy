bb4773809698570deb7e107d28403a1f8b7fa100
moe913 <masoodm197@gmail.com>
2025-11-29 23:38:58 -0500

    Fix Login Inputs: Force full width and dark autofill bg

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('Common Investor v27 Loaded');
+console.log('Common Investor v28 Loaded');
 // ===== Utilities =====
 const $ = (sel) => document.querySelector(sel);
 const $$ = (sel) => Array.from(document.querySelectorAll(sel));
