fc6681b12b6fce4a6d5b1943ed0f744b5a1eee26
moe913 <masoodm197@gmail.com>
2025-11-30 14:22:03 -0500

    Standardize Tab Names and Fields across Desktop/Mobile

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('Common Investor v46 Loaded');
+console.log('Common Investor v47 Loaded');
 // ===== Utilities =====
 const $ = (sel) => document.querySelector(sel);
 const $$ = (sel) => Array.from(document.querySelectorAll(sel));
